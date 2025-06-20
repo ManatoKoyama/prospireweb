f689a90bdc2ac1ef01211c706908de9a8c4e0cae
Manato Koyama <manato.koyama@pro-spire.co.jp>
2025-06-20 14:17:17 +0900

    初回コミット

--- src/main/java/com/prospire/prospireweb/controller/HelloController.java
+++ src/main/java/com/prospire/prospireweb/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.prospireweb.demo.controller;
+package com.prospire.prospireweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
